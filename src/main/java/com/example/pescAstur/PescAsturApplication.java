320ad1085797472e83dd933f71e2e161b879fed7
Victor Xavier Rodríguez <161498057+VictorXavierR@users.noreply.github.com>
2024-06-29 10:17:37 +0200

    Guardar el projecto de backend en github

---
+++ src/main/java/com/example/pescAstur/PescAsturApplication.java
@@ -0,0 +1,15 @@
+package com.example.pescAstur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PescAsturApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(PescAsturApplication.class, args);
+		
+	}
+
+}
---
+++ src/test/java/com/example/pescAstur/PescAsturApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pescAstur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PescAsturApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
